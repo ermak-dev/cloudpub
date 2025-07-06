489b59218e529ce9cacf1cd2c6a8742d592342c5
Anton Ermak <anton@ermak.dev>
2025-07-06 16:53:56 +0700

    Drop build folder

--- docs/build/assets/js/aba21aa0.9d11f6df.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_cloudpub_docs=self.webpackChunk_cloudpub_docs||[]).push([[742],{7093:u=>{u.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
